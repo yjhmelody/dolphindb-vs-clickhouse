47e0b74c2ea3f2961e03ca1b608d0f24897b9892
yjhmelody <jiahao.ye@dolphindb.com>
2019-06-11 13:52:49 +0800

    update cfg

--- script/clickhouse/load_csv3.sh
+++ script/clickhouse/load_csv3.sh
@@ -3,6 +3,7 @@ starttime=start$(date +%Y-%m-%d\ %H:%M:%S)
 echo $starttime 
 for ((a=1; a<=1000;a++)) do
     clickhouse-client --port 19001 --use_client_time_zone true --query="INSERT INTO test_table FORMAT CSV" < rand.csv
+    echo $a
 done 
 endtime=end$(date +"%Y-%m-%d %H:%M:%S")
 echo $endtime 
